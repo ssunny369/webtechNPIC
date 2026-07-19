5a571e35989f57c8b48a5c2f7dc72a23c24e8d28
Your Name <you@example.com>
2026-07-19 22:49:38 +0700

    email verification backend

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/signin', [AuthController::class, 'signin']);
 
+Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+    ->middleware('signed')
+    ->name('verify.email');
+Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
     Route::get('/verify', [AuthController::class, 'verify']);
