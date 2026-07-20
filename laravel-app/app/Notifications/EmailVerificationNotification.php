1afa73700e20aee602b29c09f5078871787c84f5
Your Name <you@example.com>
2026-07-20 09:22:18 +0700

    Email verification backend complete

--- laravel-app/app/Notifications/EmailVerificationNotification.php
+++ laravel-app/app/Notifications/EmailVerificationNotification.php
@@ -2,21 +2,25 @@
 
 namespace App\Notifications;
 
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use URL;
 
 class EmailVerificationNotification extends Notification
 {
     use Queueable;
 
+    private ?string $callback_url;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct($callback_url = null)
     {
-        //
+        $this->callback_url = $callback_url;
     }
 
     /**
@@ -34,21 +38,25 @@ class EmailVerificationNotification extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
+        $verificationURL = $this->verificationURL($notifiable);
+
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+            ->subject('Verify Email Address')
+            ->line('Click the button below to verify your email address.')
+            ->action('Verify Email', $this->callback_url . '?forwarded-url=' . urlencode($verificationURL))
+            ->line('If you did not create an account, no further action is required.')
+            ->line('This verification link will expire in 5 minutes.');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
+    protected function verificationURL($notifiable)
     {
-        return [
-            //
-        ];
+        return URL::temporarySignedRoute(
+            'verify.email',
+            Carbon::now()->addMinutes(5),
+            [
+                'id' => $notifiable->getKey(), // user id
+                'hash' => sha1($notifiable->getEmailForVerification()), // hash using user email
+            ]
+        );
     }
 }
