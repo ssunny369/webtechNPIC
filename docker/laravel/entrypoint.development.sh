7dda0de9e061dbe6b91c11893297a4beb2fe3a0a
Your Name <you@example.com>
2026-08-03 08:43:25 +0700

    Laravel Octane + RoadRunner

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-composer install
-wait $!
+# composer install
+# wait $!
 php artisan key:generate
 wait $!
 php artisan migrate
 wait $!
-php artisan serve --host=0.0.0.0 --port=8000
+# php artisan serve --host=0.0.0.0 --port=8000 &
+
+# php artisan queue:work --tries=3
+
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
